326fe92ea1261dfc71e706e2dc3a1c886a962e65
mohammadsalem <mohammad.salem231@gmail.com>
2021-05-28 20:05:47 +0300

    WorldFish theme

---
+++ custom_configuration/themes/knowledgearchive/theme/scripts/custom.js
@@ -0,0 +1,12 @@
+/**
+ * Created by haris on 11/21/18.
+ */
+$(document).ready(function () {
+    // var url = window.location.href;
+    // var id = url.substring(url.lastIndexOf('#') + 1);
+    // if (id == 'showfilters') {
+    //     $('.show-advanced-filters').click();
+    // }
+
+    $('.popovers').popover({html: true});
+});
