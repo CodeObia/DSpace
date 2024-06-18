540747b9fc1ddcf1330c10569572763ea9ed3071
mohammadsalem <mohammad.salem231@gmail.com>
2024-06-18 11:57:01 +0000

    https://github.com/CodeObia/MEL/issues/11157 adjust DB import script for DSpace7

--- IMPORT_DSPACE_DB.sh
+++ IMPORT_DSPACE_DB.sh
@@ -35,17 +35,16 @@ echo "$(tput setaf 6)Stopping dspace container..."$(tput sgr 0)
 docker stop dspace7
 
 echo "$(tput setaf 6)Droping database..."$(tput sgr 0)
-docker exec dspacedb dropdb -U dspace dspace
+docker exec dspacedb dropdb -U postgres dspace
 
 echo "$(tput setaf 6)Creating database..."$(tput sgr 0)
-docker exec dspacedb createdb -U dspace -O dspace --encoding=UNICODE dspace
+docker exec dspacedb createdb -U postgres -O dspace --encoding=UNICODE dspace
 
 echo "$(tput setaf 6)Creating pgcrypto extension..."$(tput sgr 0)
-docker exec dspacedb psql -U dspace dspace -c 'CREATE EXTENSION pgcrypto;'
+docker exec dspacedb psql -U postgres dspace -c 'CREATE EXTENSION pgcrypto;'
 
 echo "$(tput setaf 6)Creating dspace user..."$(tput sgr 0)
-docker exec dspacedb psql -U dspace dspace -c "CREATE USER postgres WITH PASSWORD 'postgres';"
-docker exec dspacedb psql -U dspace dspace -c 'alter user postgres superuser;'
+docker exec dspacedb psql -U postgres dspace -c 'alter user dspace superuser;'
 
 echo "$(tput setaf 6)Copying database..."$(tput sgr 0)
 docker cp dspace-$num.dump dspacedb:/
@@ -54,19 +53,19 @@ echo "$(tput setaf 6)Importing database..."$(tput sgr 0)
 docker exec dspacedb pg_restore -U postgres -d dspace /dspace-$num.dump
 
 echo "$(tput setaf 6)Vacum database..."$(tput sgr 0)
-docker exec dspacedb vacuumdb -U dspace dspace
+docker exec dspacedb vacuumdb -U postgres dspace
 
 echo "$(tput setaf 6)Updating sequences..."$(tput sgr 0)
 docker cp ../dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/update-sequences.sql dspacedb:/
 docker exec dspacedb psql -U dspace -f /update-sequences.sql dspace
 
 echo "$(tput setaf 6)Removing dspace user..."$(tput sgr 0)
-docker exec dspacedb psql -U dspace dspace -c 'alter user postgres nosuperuser;'
+docker exec dspacedb psql -U postgres dspace -c 'alter user dspace nosuperuser;'
 
 echo "$(tput setaf 6)Cleaning up..."$(tput sgr 0)
 docker exec -it dspacedb bash -c "rm dspace-$num.dump"
 docker exec -it dspacedb bash -c "rm update-sequences.sql"
-#rm dspace-$num.dump
+rm dspace-$num.dump
 
 echo "$(tput setaf 6)Starting dspace container..."$(tput sgr 0)
 docker start dspace7
