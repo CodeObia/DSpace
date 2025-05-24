60ca3a060d37b14a49bae7a93fc9c3f24a9d8a35
mohammadsalem <mohammad.salem231@gmail.com>
2025-05-24 07:42:47 +0000

    Fix DB backup directory

--- DSPACE_BACKUP.sh
+++ DSPACE_BACKUP.sh
@@ -6,7 +6,6 @@ echo "$(tput setaf 6)Creating full backup $db_name.tar.gz ...$(tput sgr 0)"
 
 cd "$SCRIPT_DIR"/data/db_backup/ || exit
 docker exec -i dspacedb pg_dump -U dspace -Fc -f /"$db_name" dspace
-cd ./data/db_backup/ || exit
 docker cp dspacedb:/"$db_name" .
 docker exec dspacedb sh -c "rm /$db_name"
 tar -czvf "$db_name".tar.gz "$db_name"
